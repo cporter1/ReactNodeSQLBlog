75fbc981bc77143d2c21fb646cd16810e6321d00
Colin Porter <coln.porter@gmail.com>
2020-10-02 14:08:43 -0700

    changes to server/index

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const app = express(); // create express app
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
+
 app.get("/api/hey", (req, res) => {
   res.send("Hello from the Node.js server!");
 });
 
 // start express server on port 5000
-app.listen(3001, () => {
+app.listen(8000, () => {
   console.log("server started on port 3001");
 });
