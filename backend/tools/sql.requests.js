1d205d7432a94dce152e6c8415971f46de772765
Eric <31486565+eric-thiem@users.noreply.github.com>
2021-03-09 23:38:31 -0800

    first commit

--- backend/tools/sql.requests.js
+++ backend/tools/sql.requests.js
@@ -28,7 +28,7 @@ async function DBcreateAccount(accEmail, accPassword, accName,) {
 async function DBcreatePost(postTitle, postAuthor, postBody, postTime, postID) {
     let DBquery = 
         `INSERT INTO Posts VALUES ('${postTitle}', '${postAuthor}',
-            '${postBody}', '${postTime}', '${postID}')`;
+            '${postBody}', '${postTime}', '${postID}', '0')`;
     
     let pool = await sql.connect(DBLogin);
     let data = await pool.request().query(DBquery);
@@ -134,6 +134,57 @@ async function DBdeleteSession(sessionID) {
     return data.recordset;
 }
 
+async function DBUpvote(id, username) {
+    let pool = await sql.connect(DBLogin);
+    let upvoteQuery = `SELECT 1 FROM sys.columns WHERE Name = '${username}'`;
+    let data;
+    try {
+        data = await pool.request().query(upvoteQuery);
+    } catch (e) {}
+
+    if(data.recordset[0] && data.recordset[0][''] === 1){
+        //User already exists on the upvote table
+        await pool.request().query(`INSERT INTO Upvotes (${username}) VALUES ('${id}')`);
+    } else {
+        //User does not already exist on the upvote table
+        await pool.request().query(`ALTER TABLE Upvotes ADD ${username} varchar(255)`)
+          .then(await pool.request().query(`INSERT INTO Upvotes (${username}) VALUES ('${id}')`));
+    }
+
+    let DBquery = `UPDATE Posts SET Upvotes = Upvotes + 1 WHERE PostID = '${id}'`;
+    await pool.request().query(DBquery);
+    pool.close();
+    sql.close();
+}
+
+async function DBDownvote(id) {
+    let DBquery = `UPDATE Posts SET Upvotes = Upvotes - 1 WHERE PostID = '${id}'`;
+
+    let pool = await sql.connect(DBLogin);
+    await pool.request().query(DBquery);
+    pool.close();
+    sql.close();
+}
+
+async function DBgetUpvoted(username) {
+    let data;
+    let DBquery = `SELECT ${username} FROM Upvotes`;
+
+    let pool = await sql.connect(DBLogin);
+    try {
+        data = await pool.request().query(DBquery);
+
+    } catch (e) {
+        pool.close();
+        sql.close();
+        return [];
+    }
+    pool.close();
+    sql.close();
+    return data.recordset;
+}
+
+
 //export the DB call functions
 exports.DBgetAccount    = DBgetAccount;
 exports.DBcreateAccount = DBcreateAccount;
@@ -146,3 +197,6 @@ exports.DBgetProfile    = DBgetProfile;
 exports.DBsaveSession   = DBsaveSession;
 exports.DBdeleteSession = DBdeleteSession;
 exports.DBgetSession    = DBgetSession;
+exports.DBUpvote        = DBUpvote;
+exports.DBDownvote      = DBDownvote;
+exports.DBgetUpvoted    = DBgetUpvoted;
